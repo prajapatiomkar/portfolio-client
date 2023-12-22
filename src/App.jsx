0150a7259bd1b133618a3fcf9aadc12b31e00769
Omkar Prajapati <72141037+prajapatiomkar@users.noreply.github.com>
2023-12-22 17:25:02 +0530

    portfolio client init.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <h1 className="flex justify-center items-center h-screen">
+      Portfolio Client!
+    </h1>
+  );
+}
